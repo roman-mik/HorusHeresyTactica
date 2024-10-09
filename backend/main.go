acc4266d0df85d04cafaadeb93372a8b1c132d71
Roman Mik <rmik@forwardfinancing.com>
2024-10-09 02:31:35 +0200

    refactor: Move legions API endpoints to separate file

--- backend/main.go
+++ backend/main.go
@@ -1,22 +1,16 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gofiber/fiber/v2"
 
-	"github.com/roman-mik/horus-heresy-tactica/models"
+	"github.com/roman-mik/horus-heresy-tactica/routes/api"
 )
-
-// GetLegions handles GET requests for the list of legions
-func GetLegions(c *fiber.Ctx) error {
-	return c.Status(http.StatusOK).JSON(models.GetLegions())
-}
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/legions", GetLegions)
+	app.Get("/api/legions", api.GetLegions)
+	app.Get("/api/legions/:id", api.GetLegionByID)
 
 	app.Listen(":3000")
 }
