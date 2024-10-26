17118fb30c1e595178ab05465adf6357c5758245
Roman Mik <it.mikroman@gmail.com>
2024-10-26 03:47:08 +0200

    feat(FE): Add NavBar component
    - Add NavBar component to the frontend
    - Create NavBar module CSS file
    - Implement NavBar functionality with links to different pages
    - Update global CSS styles for background and font colors
    - Update layout to include NavBar component
    - Update home page content

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+
 import './globals.css';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
+import NavBar from '@/components/NavBar';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -26,7 +16,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
+        <NavBar />
         {children}
       </body>
     </html>
---
+++ frontend/components/NavBar/NavBar.tsx
@@ -0,0 +1,13 @@
+import styles from './NavBar.module.css';
+
+export const NavBar: React.FC = () => {
+  return (
+    <nav className={styles.root}>
+      <a href="/legions">Legions</a>
+      <a href="/primarchs">Primarchs</a>
+      <a href="/battles">Battles</a>
+      <a href="/tactics">Tactics</a>
+      <a href="/lore">Lore</a>
+    </nav>
+  );
+};
